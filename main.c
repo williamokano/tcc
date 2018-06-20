4d9ad1d43c5ead53246f74dcdc367b5515d6ac08
William Okano <williamokano@gmail.com>
2018-06-20 01:36:53 -0300

    Added readme and some references

--- main.c
+++ main.c
@@ -1,27 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 #include "headers/graphics.h"
 
 int main() {
     initWindow();
-    int columnOffset = 0;
-    
-    setColor(point3d(0xFF, 0x00, 0xFF));
-    triangle(
-        point2d(100, 100), 
-        point2d(200, 200), 
-        point2d(200, 100)
-    );
-
-    setColor(point3d(0x00, 0xFF, 0xFF));
-    point(point2d(500, 500), 50);
-
-    while(true) {
-        // clear();
-        // point(point2d(columnOffset, columnOffset), 50);
-        // columnOffset += 20;
-        sleep_cp(2);
-    }
+    point(point2d(100, 100), 30);
+    getchar();
     return 0;
 }
--- readme.md
+++ readme.md
@@ -7,7 +7,14 @@ To run on linux, first install glfw3.
 If you're using ubuntu like platform, just run `sudo apt-get install libglfw3-dev libglfw3`  
 
 **Running on windows**:
+On windows you need to install mingw-w64 due the need to work with threads with tinycthreads.
+[https://mingw-w64.org/](https://mingw-w64.org/)
+
 just run `compile.bat`  
 
 **Running on linux**:
 just run `compile.sh`  
+
+## References
+BGI Lib - https://www.cs.colorado.edu/~main/bgi/doc/  
+OpenGL Tutorials - https://www.youtube.com/watch?v=BrKIWYylv3Q&t=272s  
