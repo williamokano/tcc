001915b4a41e82a77edb70048896d6a64e415162
William Johnson dos Santos Okano <williamokano@gmail.com>
2018-06-21 14:28:11 -0300

    Added some checking for the linux compile command

--- compile.sh
+++ compile.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
-gcc -o bin/main main.c src/graphics.c src/point.c src/tinycthread.c -lGL -lX11 -lpthread -lglfw
+
+# Create bin folder if not found
+if [ ! -d "./bin" ];then
+    mkdir -p ./bin
+fi
+
+# Clean bin folder if present
+if [ -f "./bin/main" ];then
+    rm ./bin/main
+fi
+
+gcc -o bin/main main.c \
+    src/graphics.c \
+    src/point.c \
+    src/tinycthread.c \
+    -lGL -lX11 -lpthread -lglfw
