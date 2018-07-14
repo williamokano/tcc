05677a17663f3ab209620a6285eecebe08ee01fc
William Johnson dos Santos Okano <williamokano@gmail.com>
2018-07-14 01:05:39 -0300

    /usr/local/bin/gaacop

--- test.c
+++ test.c
@@ -4,9 +4,19 @@
 void showMenu();
 
 int main() {
-    inicializarBiblioteca(1366, 768);
+    inicializarBiblioteca(500, 500);
 
-    
+    definirCor(0, 0, 255);
+    poligonoRegular(100, 100, 50, 3);
+
+    definirCor(255, 0, 0);
+    poligonoRegular(200, 200, 65, 4);
+
+    definirCor(0, 255, 0);
+    poligonoRegular(300, 300, 38, 5);
+
+    definirCor(255, 255, 0);
+    poligonoRegular(400, 400, 50, 6);
 
     getchar();
     return 0;
